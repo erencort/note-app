81efec3fddc7671e997fa0128e3fca2ca6ac973a
Eren <erencort1@gmail.com>
2023-06-22 23:13:04 +0300

    responsive

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -39,7 +39,7 @@ function Note({ itemIndex, item, id }) {
         onChange={(e) => handleChange(e)}
         defaultValue={item.content}
         rows="10"
-        className="mx-auto border-2 p-2 w-1/4 border-black bg-gray-600 rounded-xl"
+        className="mx-auto border-2 p-2 min-w-fit w-1/4 border-black bg-gray-600 rounded-xl text-lg"
       ></textarea>
       <div>
         <button
@@ -52,7 +52,6 @@ function Note({ itemIndex, item, id }) {
     </div>
   );
 }
-
 //className="bg-red-400 p-2  rounded-lg mt-1 mb-7"
 //class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
 export default Note;
